ee5ebbbd1e77d5a53b5279719a7dce1b1cc8dc73
abopanda2003 <artekloc4@gmail.com>
2022-04-05 03:53:29 -0500

    04-05 04 a.m

--- contracts/interfaces/ISmartAchievement.sol
+++ contracts/interfaces/ISmartAchievement.sol
@@ -9,20 +9,29 @@ interface ISmartAchievement {
         uint256           goldenTreeRewards;   // SMTC golden tree rewards
         uint256           passiveShare;        // Passive share percent
         uint256           availableTitles;     // Titles available
-        uint256[]         chestSMTRewards;
-        uint256[]         chestSMTCRewards;
+        uint256[]         chestSMTRewardPool;
+        uint256[]         chestSMTCRewardPool;        
+    }
+
+    struct UserInfo {
+        uint256[] chestRewards; // 0: SMT,  1: SMTC
+        uint256 checkRewardUpdated;
+        uint256[] surprizeRewards; // 0: SMT, 1: SMTC
+        uint256 nobleRewards;
+        uint256 farmRewards;
     }
 
     function addFarmDistributor(address) external;
     function claimReward() external;
-    function claimChestReward() external;
     function claimNobleReward(uint256) external;
     function claimFarmReward(uint256) external;
+    function claimChestSMTReward(uint256) external;
+    function claimChestSMTCReward(uint256) external;
     function claimSurprizeSMTReward(uint256) external;
     function claimSurprizeSMTCReward(uint256) external;
     function distributeToNobleLeaders(uint256) external;
     function distributeToFarmers(uint256) external;
-    function distributeSurprizeReward(address) external;
+    function distributeSurprizeReward(address, uint256) external;
     function notifyGrowth(address account, uint256 oldGrowth, uint256 newGrowth) external returns(bool);
     function removeFarmDistributor(address) external;
     function swapDistribute() external;
